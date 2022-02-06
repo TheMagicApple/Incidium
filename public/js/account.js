b3a32197fa7eb03f99937185a28cd886724e471e
potato-potato-potato-potato <jgao666@gmail.com>
2022-02-06 13:10:02 -0800

    me is stupid

--- public/js/account.js
+++ public/js/account.js
@@ -3,10 +3,12 @@ const accountElement = document.querySelector("#Account")
 function setUsername() {
   const request = new XMLHttpRequest();
   request.open("GET", "http://www.incidium.tech/api/login/info", true)
-  request.onload = () => {
-    console.log(request.response);
-    accountElement.innerText = `Welcome ${JSON.parse(request.response).username}`;
-  }
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       console.log(request.responseText)
+      accountElement.innerText = `Welcome ${JSON.parse(request.responseText).username}`;
+    }
   request.send();
 
   
