3d7ba23b7234abe941b798227aeb491fbcf17d17
potato-potato-potato-potato <jgao666@gmail.com>
2022-02-06 12:44:03 -0800

    me is stupid

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -8,7 +8,7 @@ router.use(express.json());
 router.use(cookieParser());
 
 router.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://incidium.tech/");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
