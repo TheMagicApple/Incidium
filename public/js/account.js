58b9c288f16eb8b859ffd8a2e95039a3345d0bfa
potato-potato-potato-potato <jgao666@gmail.com>
2022-02-06 13:15:20 -0800

    me is stupid

--- public/js/account.js
+++ public/js/account.js
@@ -5,7 +5,8 @@ function setUsername() {
   request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-    accountElement.innerHTML = JSON.parse(this.responseText)
+       console.log(this.responseText)
+    accountElement.innerHTML = `Welcome ${JSON.parse(this.responseText).username}`
     }
   }
   request.open("GET", "http://www.incidium.tech/api/login/info", true)
