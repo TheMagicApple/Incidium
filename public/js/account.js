6718ed3cd5c8357a451b2061c93f579a2abcf6df
potato-potato-potato-potato <jgao666@gmail.com>
2022-02-06 13:06:18 -0800

    me is stupid

--- public/js/account.js
+++ public/js/account.js
@@ -3,10 +3,10 @@ const accountElement = document.querySelector("#Account")
 function setUsername() {
   const request = new XMLHttpRequest();
   request.open("GET", "http://www.incidium.tech/api/login/info", true)
+  request.onload = () => {
+    accountElement.innerText = `Welcome ${JSON.parse(request.response).username}`;
+  }
   request.send();
 
-  request.onload = () => {
-    console.log(this.response)
-    accountElement.innerText = `Welcome ${JSON.parse(this.response).username}`;
-  }
+  
 }
