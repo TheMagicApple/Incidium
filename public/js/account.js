798e23818cdc431337174dd25be0e6cc16703567
owen gao <50187784+potato-potato-potato-potato@users.noreply.github.com>
2022-02-06 17:53:51 +0000

    update

--- public/js/account.js
+++ public/js/account.js
@@ -1,3 +1,4 @@
-var express=require("express");
-var username=req.cookies.get("username");
+var cookieParser = require('cookie-parser');
+
+var username=req.cookies("username");
 document.getElementById("Account").innerHTML="Welcome "+username;
