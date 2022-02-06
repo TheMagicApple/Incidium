11904ec39c68ac4fcce8d6f73c54ac565b2d589d
potato-potato-potato-potato <jgao666@gmail.com>
2022-02-06 11:58:40 -0800

    re

--- public/js/signup.js
+++ public/js/signup.js
@@ -29,7 +29,7 @@ function signUp() {
     requst.open("POST", "/api/users")
     requst.setRequestHeader("Content-type","application/json;charset=UTF-8")
     requst.send(JSON.stringify(requestBody))
-    console.log(resuest);
+    console.log(request);
     console.log(requestBody)
     console.log(requst);
 }
